890e087875c90725987f1cde13329464920d450c
Andrew Dolzhenkov <doland75@gmail.com>
2025-12-23 15:45:33 +0300

    Revert "ver 0.1"
    This reverts commit e5737647e72b4360f19cbb6545a86cffbf87beab.

--- src/main/java/com/example/bankcards/repository/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankcards.repository.users;
-
-import com.example.bankcards.entity.users.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
